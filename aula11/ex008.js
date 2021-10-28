3f4bfc9208dc719d6c127b0b42ecc6de2c0beadd
luiz-fg <luiz.gomes@universo.univates.br>
2021-10-27 21:19:38 -0300

    Vídeo 16 - Aula 11 - Condiçoes Parte 1

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+let vel = 60
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+if(vel > 60)
+{
+  console.log('Você ultrapassou o limite de velocidade. MULTADO!')
+}
+
+console.log('Ao dirigir use sempre o cinto de segurança')
---
+++ aula11/ex009.js
@@ -0,0 +1,12 @@
+var pais = 'Brasil'
+
+console.log(`Reside em: ${pais}`)
+
+if(pais == 'Brasil')
+{
+  console.log('você é brasileiro!')
+}
+else
+{
+  console.log('você é estrangeiro!')
+}
