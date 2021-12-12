47940b6a001cb610325001bae396861b96ea775c
luiz-fg <luiz.gomes@universo.univates.br>
2021-12-12 10:55:26 -0300

    Vídeo 19 - Aula 13 - Repetições Parte 1

---
+++ aula13/ambiente.js
@@ -0,0 +1,27 @@
+// Usando método sem repetição
+/*
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+*/
+
+let c = 1;
+
+while(c <= 6)
+{
+    console.log(`Entrei no loop ${c} Vezes`)
+    c++
+}
+
+let d = 1;
+
+do
+{
+    console.log(`doWhile ${d} Vezes`)
+    d++
+} while(d <= 6)
