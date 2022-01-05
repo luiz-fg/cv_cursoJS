60979ac8cd10f1b06d24154394c2171b81b4d62d
luiz-fg <luiz.gomes@universo.univates.br>
2022-01-05 20:30:30 -0300

    Vídeo 25 - Exercicios pt-01

--- exercicios pt-02/js/app.js
+++ exercicios pt-02/js/app.js
@@ -9,20 +9,20 @@ function horaDia()
     if(hora < 13 )
     {
            txt.innerHTML = `Agora são ${hora} horas.`
-           img.src = '../hora-dia/img/manha.jpg'
+           img.src = 'img/manha.jpg'
            document.body.style.background = '#BDB76B'
            
     }
     else if (hora < 18)
     {
         txt.innerHTML = `Agora são ${hora} horas.`
-        img.src = '../hora-dia/img/tarde.jpg'
+        img.src = 'img/tarde.jpg'
         document.body.style.background = '#CD853F'
     }
     else if(hora < 23)
     {
         txt.innerHTML = `Agora são ${hora} horas.`
-        img.src = '../hora-dia/img/noite.jpg'
+        img.src = 'img/noite.jpg'
         document.body.style.background = '#FF8C00'
     }
     else if( hora > 0 && hora < 6)
